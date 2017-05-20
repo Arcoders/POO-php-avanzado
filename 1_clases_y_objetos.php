da3ce36aab49fd4d8f5f89f0b0fe8c9be12c18fb
Ismael Haytam <anonismaa@gmail.com>
2017-05-21 00:20:42 +0200

    Clases y objetos primero ejemplo

---
+++ 1_clases_y_objetos.php
@@ -0,0 +1,23 @@
+<?php
+
+class Person {
+
+    public $firstName;
+    public $lastName;
+
+    public function __construct($firstName, $lastName) {
+        $this->firstName = $firstName;
+        $this->lastName = $firstName;
+    }
+
+    public function fullName() {
+        return $this->firstName. ' ' .$this->lastName;
+    }
+
+}
+
+$persona_uno = new Person('Victor', 'Ruvira');
+
+$persona_dos = new Person('Marta', 'Lopez');
+
+echo "{$persona_uno->fullName()} es amigo de {$persona_dos->fullName()}";
