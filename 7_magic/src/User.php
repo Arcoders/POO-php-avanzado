1a8f5fbd14165134afea448937002f49f5d0c4ed
Ismael Haytam <anonismaa@gmail.com>
2017-05-24 16:19:29 +0200

    Llamadas dinámicas a métodos

--- 7_magic/src/User.php
+++ 7_magic/src/User.php
@@ -2,59 +2,12 @@
 
 namespace Arcoders;
 
-class User
+class User extends Model
 {
 
-    protected $attributes = [];
-
-    public function __construct(array $attributes = [])
+    public function getFirstNameAttribute($value)
     {
-         $this->fill($attributes);
-    }
-
-    public function getFirstNameAttribute()
-    {
-        return strtoupper($this->first_name);
-    }
-
-    public function fill(array $attributes = [])
-    {
-        $this->attributes = $attributes;
-    }
-
-    public function getAttributes()
-    {
-        return $this->attributes;
-    }
-
-    public function getAttribute($name)
-    {
-        return $this->attributes[$name] ?? null;
-    }
-
-    public function setAttribute($name, $value)
-    {
-        $this->attributes[$name] = $value;
-    }
-
-    public function __set($name, $value)
-    {
-        $this->setAttribute($name, $value);
-    }
-
-    public function __get($name)
-    {
-        return $this->getAttribute($name);
-    }
-
-    public function __isset($name)
-    {
-        return isset($this->attributes[$name]);
-    }
-
-    public function __unset($name)
-    {
-        unset($this->attributes[$name]);
+        return strtoupper($value);
     }
 
 }
